ec02cbc1206a0a4a33938d5e83cf8679f8278051
SJDeshmukh <sudhanshudeshmukh5894@gmail.com>
2026-01-24 16:51:05 +0530

    againcorrected render.yml

--- web-dashboard/src/config.js
+++ web-dashboard/src/config.js
@@ -1,2 +1,6 @@
-const BACKEND_URL = (import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:5001').replace(/\/$/, '');
+let backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:5001';
+if (!backendUrl.startsWith('http')) {
+  backendUrl = `https://${backendUrl}`;
+}
+const BACKEND_URL = backendUrl.replace(/\/$/, '');
 export const API_URL = `${BACKEND_URL}/api`;
