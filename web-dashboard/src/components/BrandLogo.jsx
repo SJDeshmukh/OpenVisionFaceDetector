c3f32e382ad74ad55560fe517b19ffbdf13a1c23
Sudhanshu Deshmukh <sudhanshu@expenzing.com>
2026-09-03 14:05:38 +0530

    fix: remove inner plate from XChat icon

--- web-dashboard/src/components/BrandLogo.jsx
+++ web-dashboard/src/components/BrandLogo.jsx
@@ -1,5 +1,5 @@
-const BrandLogo = ({ className = 'h-9 w-9', alt = 'TapInX' }) => (
-  <img src="/tapinx-logo.svg" alt={alt} className={`shrink-0 object-contain ${className}`} />
+const BrandLogo = ({ className = 'h-9 w-9', alt = 'TapInX', symbolOnly = false }) => (
+  <img src={symbolOnly ? '/tapinx-symbol.svg' : '/tapinx-logo.svg'} alt={alt} className={`shrink-0 object-contain ${className}`} />
 );
 
 export default BrandLogo;
